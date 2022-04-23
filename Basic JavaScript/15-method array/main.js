7d9c73c9c621d336fa6159ba0f8db28ef9765a74
rajalamofficial <rajalamofficial@gmail.com>
2022-04-23 21:18:25 +0700

    update

---
+++ Basic JavaScript/15-method array/main.js
@@ -0,0 +1,16 @@
+let nama = ['Azura', 'Aramu', 'San'];
+
+// method pada array
+
+// push: menambahkan array di akhir
+// pop: menghapus array di akhir
+nama.push('Yin', 'Yan');
+nama.pop();
+
+// unshift: menambahkan array di awal
+// shift: menghapus array di awal
+nama.unshift('Luffy', 'Zoro');
+nama.shift();
+
+// join: menampilkan semua isi array    
+console.log(nama.join(' - '));

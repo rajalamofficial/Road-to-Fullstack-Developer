5f456bcb4f2fc0cacd989704a8600ded0de7af76
rajalamofficial <rajalamofficial@gmail.com>
2022-04-25 23:40:53 +0700

    update

--- Basic JavaScript/17-latihan/main.js
+++ Basic JavaScript/17-latihan/main.js
@@ -1,35 +1,51 @@
-let penumpang = ['Azura', undefined, 'Ya'];
+let bangku = ['Azura', undefined, 'Aramu'];
 let tambahPenumpang = function(namaPenumpang){
-    // jika angkot kosong
-        if(penumpang.length == 0){
-            // tambah penumpang di awal array
-            penumpang.unshift(namaPenumpang);
-            // kembalikan isi array & keluar dari function
-            return penumpang;
-        } else {
-    // (else) telusuri kursi dari awal
-            for(let i = 0; i < penumpang.length; i++){
-            // jika ada kursi kosong
-                if(penumpang[i] == undefined){
-                    // tambah penumpang di kursi tersebut
-                    penumpang[i] = namaPenumpang;
-                    // kembalikan isi array & keluar dari function
-                    return penumpang;
-                }
-                // jika sudah ada nama yang sama 
-                 else if(penumpang[i] == namaPenumpang){
-                    // tampilkan pesan kesalahannya
-                    console.log(`${namaPenumpang} sudah berada di dalam Angkot!`);
-                    // kembalikan isi array & keluar dari function
-                    return penumpang;
-                }
-                // jika seluruh kursi sudah terisi
-                else if(i == penumpang.length - 1){
-                    // tambah penumpang di akhir array
-                    penumpang.push(namaPenumpang);
-                    // kembalikan isi array & keluar dari function
-                    return penumpang;
-                }
+    // jika angkot kosong, penumpang duduk di kursi pertama
+    if(bangku.length == 0){
+        bangku.unshift(namaPenumpang);
+        return bangku;
+    } else {
+        for(let i = 0; i < bangku.length; i++){
+            // jika ada kursi kosong, penumpang selanjutnya duduk di kursi tersebut
+            if(bangku[i] == undefined){
+                bangku[i] = namaPenumpang;
+                return bangku;
+            }
+
+            // jika ada nama penumpang yang sama, tampilkan pesan peringatan
+            else if(bangku[i] == namaPenumpang){
+                console.log(`"${namaPenumpang}" sudah berada di dalam angkot!`);
+                return false;
+            }
+
+            // jika kursi sudah terisi secara urut, penumpang selanjutnya duduk di kursi terakhir secara urut
+            else if(i == bangku.length - 1){
+                bangku.push(namaPenumpang);
+                return bangku;
             }
         }
+    }
+}
+
+let hapusPenumpang = function(namaPenumpang){
+    // jika angkot kosong, tampilkan pesan bahwa angkot kosong
+    if(bangku.length == 0){
+        console.log(`Angkot sedang kosong!, "${namaPenumpang}" tidak ada di dalam angkot!`);
+        return bangku;
+    } else{
+            for(let i = 0; i < bangku.length; i++){
+            // jika ada penumpang yang namanya sesuai, hapus nama penumpang pada array dengan memberi nilai undefined
+            if(bangku[i] == namaPenumpang){
+                bangku[i] = undefined;
+                return bangku;
+            }
+        
+            // jika tidak ada penumpang yang namanya tidak sesuai, tampilkan pesan kesalahannya
+            else if(i == bangku.length - 1){
+                console.log(`"${namaPenumpang}" tidak ada di dalam angkot!`);
+                return bangku;
+            }
+        }
+    }
+
 }
